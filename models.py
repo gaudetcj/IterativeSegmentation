25252753d66bb9709ffbc2e7d6b0f985b039f7a9
Chase <gaudetcj@gmail.com>
2016-12-17 18:02:24 -0600

    Add training file to train on Nerve Segmentation using that pre-trained CNN.

--- models.py
+++ models.py
@@ -5,18 +5,68 @@ Created on Sat Dec 17 17:03:57 2016
 @author: Chase
 """
 
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers import Input, merge, SpatialDropout2D
+from keras.layers import Convolution2D, AveragePooling2D, UpSampling2D
 from keras.models import Model
-from keras.layers import Input, merge
-from keras.layers import Convolution2D
 from keras.optimizers import Adam
+
+
+def get_cnn_model(img_rows, img_cols):
+    input = Input(shape=(1, img_rows, img_cols))
+    
+    conv1 = Convolution2D(32, 3, 3, border_mode='same', init='he_normal')(input)
+    conv1 = LeakyReLU()(conv1)
+    conv1 = SpatialDropout2D(0.2)(conv1)
+    conv1 = Convolution2D(32, 3, 3, border_mode='same', init='he_normal')(conv1)
+    conv1 = LeakyReLU()(conv1)
+    conv1 = SpatialDropout2D(0.2)(conv1)
+    pool1 = AveragePooling2D(pool_size=(2,2))(conv1)
+    
+    conv2 = Convolution2D(64, 3, 3, border_mode='same', init='he_normal')(pool1)
+    conv2 = LeakyReLU()(conv2)
+    conv2 = SpatialDropout2D(0.2)(conv2)
+    conv2 = Convolution2D(64, 3, 3, border_mode='same', init='he_normal')(conv2)
+    conv2 = LeakyReLU()(conv2)
+    conv2 = SpatialDropout2D(0.2)(conv2)
+    pool2 = AveragePooling2D(pool_size=(2,2))(conv2)
+    
+    conv3 = Convolution2D(128, 3, 3, border_mode='same', init='he_normal')(pool2)
+    conv3 = LeakyReLU()(conv3)
+    conv3 = SpatialDropout2D(0.2)(conv3)
+    conv3 = Convolution2D(128, 3, 3, border_mode='same', init='he_normal')(conv3)
+    conv3 = LeakyReLU()(conv3)
+    conv3 = SpatialDropout2D(0.2)(conv3)
+    
+    comb1 = merge([conv2, UpSampling2D(size=(2,2))(conv3)], mode='concat', concat_axis=1)
+    conv4 = Convolution2D(64, 3, 3, border_mode='same', init='he_normal')(comb1)
+    conv4 = LeakyReLU()(conv4)
+    conv4 = SpatialDropout2D(0.2)(conv4)
+    conv4 = Convolution2D(64, 3, 3, border_mode='same', init='he_normal')(conv4)
+    conv4 = LeakyReLU()(conv4)
+    conv4 = SpatialDropout2D(0.2)(conv4)
+    
+    comb2 = merge([conv1, UpSampling2D(size=(2,2))(conv4)], mode='concat', concat_axis=1)
+    conv5 = Convolution2D(32, 3, 3, border_mode='same', init='he_normal')(comb2)
+    conv5 = LeakyReLU()(conv5)
+    conv5 = SpatialDropout2D(0.2)(conv5)
+    conv5 = Convolution2D(32, 3, 3, border_mode='same', init='he_normal')(conv5)
+    conv5 = LeakyReLU()(conv5)
+    conv5 = SpatialDropout2D(0.2)(conv5)
+    
+    output = Convolution2D(1, 1, 1, activation='sigmoid')(conv5)
+
+    model = Model(input=input, output=output)
+    model.compile(optimizer=Adam(lr=3e-4), loss='binary_crossentropy')
+    return model
 
 
 def get_iterative_model(img_rows, img_cols):
     # the heatmap from CNN
     input1 = Input(shape=(1, img_rows, img_cols))
     
-    # the image heatmap was generated from
-    input2 = Input(shape=(3, img_rows, img_cols))
+    # the image heatmap was generated from (change 1 to 3 for color images)
+    input2 = Input(shape=(1, img_rows, img_cols))
     
     # these are shared layer declarations
     convh1 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')
---
+++ train.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 17 17:31:46 2016
+
+@author: Chase
+"""
+
+import cv2
+import models
+import numpy as np
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+
+
+img_rows = 80
+img_cols = 100
+
+
+def load_train_data():
+    imgs_train = np.load('imgs_train.npy')
+    imgs_mask_train = np.load('imgs_mask_train.npy')
+    return imgs_train, imgs_mask_train
+    
+    
+def load_test_data():
+    imgs_test = np.load('imgs_test.npy')
+    return imgs_test
+    
+    
+def preprocess(imgs):
+    imgs_p = np.ndarray((imgs.shape[0], imgs.shape[1], img_rows, img_cols), dtype=np.uint8)
+    for i in range(imgs.shape[0]):
+        imgs_p[i, 0] = cv2.resize(imgs[i, 0], (img_cols, img_rows), interpolation=cv2.INTER_CUBIC)
+    return imgs_p
+
+
+def train_and_predict():
+    print('-'*30)
+    print('Loading and preprocessing train data...')
+    print('-'*30)
+    imgs_train, imgs_mask_train = load_train_data()
+
+    imgs_train = preprocess(imgs_train)
+    imgs_mask_train = preprocess(imgs_mask_train)
+
+    imgs_train = imgs_train.astype('float32')
+    mean = np.mean(imgs_train)  # mean for data centering
+    std = np.std(imgs_train)  # std for data normalization
+
+    imgs_train -= mean
+    imgs_train /= std
+
+    imgs_mask_train = imgs_mask_train.astype('float32')
+    imgs_mask_train /= 255.  # scale masks to [0, 1]
+    
+    print('-'*30)
+    print('Creating and compiling CNN model...')
+    print('-'*30)
+    model = models.get_cnn_model(img_rows, img_cols) 
+    
+    print('-'*30)
+    print('Loading saved weights into CNN model...')
+    print('-'*30)
+    model.load_weights('weights_cnn.hdf5')
+
+    print('-'*30)
+    print('Predicting masks on test data using CNN model...')
+    print('-'*30)
+    imgs_cnn_mask = model.predict(imgs_train, verbose=1)
+    
+    print('-'*30)
+    print('Creating and compiling iterative model...')
+    print('-'*30)
+    model = models.get_iterative_model(img_rows, img_cols)
+    
+    print('-'*30)
+    print('Begin training...')
+    print('-'*30)
+    callbacks = [
+        EarlyStopping(monitor='loss', patience=5, verbose=0),
+        ModelCheckpoint('weights_iter.hdf5', monitor='loss', save_best_only=True)
+    ]
+    model.fit([imgs_train, imgs_cnn_mask], imgs_mask_train, batch_size=4, nb_epoch=100, verbose=1, shuffle=True,
+              callbacks=callbacks)
+              
+    print('-'*30)
+    print('Loading and preprocessing test data...')
+    print('-'*30)
+    imgs_test = load_test_data()
+    imgs_test = preprocess(imgs_test)
+
+    imgs_test = imgs_test.astype('float32')
+    imgs_test -= mean
+    imgs_test /= std
+
+    print('-'*30)
+    print('Loading saved weights...')
+    print('-'*30)
+    model.load_weights('weights_iter.hdf5')
+
+    print('-'*30)
+    print('Predicting masks on test data using iterative model...')
+    print('-'*30)
+    imgs_mask_test = model.predict(imgs_test, verbose=1)
+    np.save('imgs_mask_test.npy', imgs_mask_test)
+
+
+if __name__ == '__main__':
+    train_and_predict()
